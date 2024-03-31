fafe691662e9af60b965e11d31da666c5b8d7e32
Edi <73744715+edyta-cymer@users.noreply.github.com>
2024-03-31 16:28:39 +0000

    Add ft_striteri.c

---
+++ ft_striteri.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+
+	i = 0;
+	if (s != NULL)
+	{
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
+}
