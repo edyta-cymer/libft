9aa4c3e019f64c3af0601a11974197ce2010719f
Edyta Cymer <ecymer@c3r11s3.42warsaw.pl>
2024-03-31 18:06:28 +0200

    modified ft_putnbr_fd and ft_split

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,26 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecymer <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 17:48:53 by ecymer            #+#    #+#             */
+/*   Updated: 2024/03/31 17:59:18 by ecymer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_putnbr(int n, int fd) {
-  if (n == -2147483648)
-    write(fd, "-2147483648", 11);
-  else {
-    if (n < 0) {
-      ft_putchar_fd('-');
-      n *= -1;
-    }
-    if (n >= 10) {
-      ft_putnbr(n / 10, fd);
-      ft_putnbr(n % 10, fd);
-    } else {
-      ft_putchar_fd(n + 48);
-    }
-  }
+void	ft_putnbr(int n, int fd)
+{
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr(n / 10, fd);
+			ft_putnbr(n % 10, fd);
+		}
+		else
+		{
+			ft_putchar_fd(n + 48, fd);
+		}
+	}
 }
-/*
+
 int	main(void)
 {
-        int a = 45678;
-        int fd = 1;
-        ft_putnbr(a, fd);
+	int	a;
+	int	fd;
+
+		a = 45678;
+		fd = 1;
+		ft_putnbr(a, fd);
 }
-*/
+
